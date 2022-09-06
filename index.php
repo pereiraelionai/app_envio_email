52dfadc60561ca9466a3d9a7e5f7fc291abd1bf9
pereiraelionai <pereira.elionai92@gmail.com>
2022-09-06 16:46:04 -0300

    Implementando melhorias de feedback para o usuário na página index

--- index.php
+++ index.php
@@ -22,24 +22,107 @@
   				
 					<div class="card-body font-weight-bold">
 						<form action="processa_envio.php" method="POST">
+							
+						<!--Campo nome-->
+							<?php
+								if (isset($_GET['nome']) and $_GET['nome'] == '') {
+							?>
 							<div class="form-group">
 								<label for="nome">Nome</label>
-								<input name="nome" type="text" class="form-control" id="para" placeholder="Digite seu nome">
-							</div>
+								<input name="nome" type="text" class="form-control is-invalid" id="para" placeholder="Digite seu nome">
+								<div class="invalid-feedback">
+									Digite um nome válido
+								</div>
+							</div>	
+							<?php
+								} else {
+							?>
+								<div class="form-group">
+									<label for="nome">Nome</label>
+									<input name="nome" type="text" class="form-control" id="para" placeholder="Digite seu nome">
+								</div>
+							<?php
+								}
+							?>
+							<!--Campo Para-->
+							<?php
+								if (isset($_GET['para']) and $_GET['para'] == '') {
+							?>
+							<div class="form-group">
+								<label for="para">Para</label>
+								<input name="para" type="text" class="form-control is-invalid" id="para" placeholder="joao@dominio.com.br">
+								<div class="invalid-feedback">
+									Digite um destinatário válido
+								</div>
+							</div>	
+
+							<?php
+								} else {
+							?>
 							<div class="form-group">
 								<label for="para">Para</label>
 								<input name="para" type="text" class="form-control" id="para" placeholder="joao@dominio.com.br">
 							</div>
+							<?php
+								}
+							?>
+
+							<!--Campo Assunto-->
+							<?php
+								if (isset($_GET['assunto']) and $_GET['assunto'] == '') {
+							?>
+							
+							<div class="form-group">
+								<label for="assunto">Assunto</label>
+								<input name="assunto" type="text" class="form-control is-invalid" id="assunto" placeholder="Assundo do e-mail">
+								<div class="invalid-feedback">
+									Digite o assunto
+								</div>
+							</div>								
+							
+
+							<?php
+								} else {
+							?>
 
 							<div class="form-group">
 								<label for="assunto">Assunto</label>
 								<input name="assunto" type="text" class="form-control" id="assunto" placeholder="Assundo do e-mail">
 							</div>
+							<?php
+								}
+							?>
+							<!--Campo mensagem-->
+							
+							<?php
+								if (isset($_GET['msg']) and $_GET['msg'] == '') {
+							?>
+							
+							<div class="form-group">
+								<label for="mensagem">Mensagem</label>
+								<textarea name="mensagem" class="form-control is-invalid" id="mensagem"></textarea>
+								<div class="invalid-feedback">
+									Digite uma mensagem
+								</div>
+							</div>						
 
+
+							<?php
+								} else {
+							?>
 							<div class="form-group">
 								<label for="mensagem">Mensagem</label>
 								<textarea name="mensagem" class="form-control" id="mensagem"></textarea>
 							</div>
+							<?php
+								}
+							?>
+
+							<?php
+							if (isset($_GET['form'])) {
+								echo '<p style="color: red">Revise os campos acima</p>';
+							}
+							?>
 
 							<button type="submit" class="btn btn-primary btn-lg">Enviar Mensagem</button>
 						</form>
